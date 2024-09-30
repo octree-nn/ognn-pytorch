7e2392296ec0e49776170d2dd812fdcad7ee6dbf
wang-ps <wangps@hotmail.com>
2024-09-30 16:36:16 +0800

    Update comments

--- ognn/nn.py
+++ ognn/nn.py
@@ -165,6 +165,7 @@ class GraphPad(torch.nn.Module):
     super().__init__()
 
   def forward(self, x: torch.Tensor, octree: OctreeD, depth: int):
+    # pad zeros to get features for the original octree nodes
     octree_mask = octree.graphs[depth].octree_mask
     out = x.new_zeros(octree_mask.shape[0], x.shape[1])
     out[octree_mask] = x  # pad zeros for internal octree nodes
