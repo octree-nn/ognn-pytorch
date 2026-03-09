9b60911ec0d2ec425160e53b03957519fd161835
Raymond Zhou <raymondzhoupku@gmail.com>
2026-03-09 03:24:30 +0000

    fall back to setup.py for backward compatibility, add pandas to requirements for triton.benchmark module

---
+++ setup.py
@@ -0,0 +1,35 @@
+# --------------------------------------------------------
+# Dual Octree Graph Neural Networks
+# Copyright (c) 2024 Peng-Shuai Wang <wangps@hotmail.com>
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Peng-Shuai Wang
+# --------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+__version__ = '1.1.2'
+
+with open("README.md", "r", encoding="utf-8") as fid:
+  long_description = fid.read()
+
+setup(
+    name='ognn',
+    version=__version__,
+    author='Peng-Shuai Wang',
+    author_email='wangps@hotmail.com',
+    url='https://github.com/octree-nn/ognn-pytorch',
+    description='Dual Octree Graph Neural Networks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['torch', 'numpy', 'ocnn', 'easydict'],
+    python_requires='>=3.6',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
